2737b2b53f88b1c9ac8bb51131cccf915621e475
Willian Paixao <willian@ufpa.br>
2017-12-07 10:51:12 +0100

    Create geth.yml.
    * Intall Go and Geth binaries for ARMv6.
    * Add pre-commit.
    * Rename main.yml and update run.sh.
    * Add an ansible config file.

--- playbooks/run.sh
+++ playbooks/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#: TITLE: "run.sh"
+#: AUTHOR: "Willian Paixao <willian.paixao@ellmount.se>"
 
+# Define your local variables.
 ENV=".env"
 INVENTORY="hosts.yml"
-PLAYBOOK="main.yml"
+PLAYBOOK="site.yml"
+TAGS="geth"
 
+# Creating and activating your virtualenv with all the dependencies.
 if [ -z "${VIRTUAL_ENV}" ];
 then
   if [ -d "${ENV}" ];
@@ -14,9 +19,23 @@ then
     source ${ENV}/bin/activate
     pip install -r requirements.txt
   fi
-fi	
+fi
 
-ansible-playbook \
-  --verbose \
+# Test connectivity with the targeted hosts.
+ansible --module-name=ping \
   --inventory-file="${INVENTORY}" \
-  "${PLAYBOOK}"
+  all
+
+if [ $? -eq 0 ]
+then
+  # Running ansible-playbook with many arguments.
+  # NOTE: The arguments '--limit' and '--tags' are optionals.
+  # NOTE: The argument '--check' will perform a "Dry Run" check. It's also
+  # optional.
+  ansible-playbook \
+    --verbose \
+    --ask-become-pass \
+    --inventory-file="${INVENTORY}" \
+    --tags="${TAGS}" \
+    "${PLAYBOOK}"
+fi
